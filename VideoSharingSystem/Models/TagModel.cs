eb7691eb5d96e5a3bc0bf112bc00cc89e48a6871
JanMalawski <janmalawski2@gmail.com>
2019-12-18 16:34:19 +0100

    Created Models

---
+++ VideoSharingSystem/Models/TagModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoSharingSystem.Models
+{
+    public class TagModel
+    {
+        public int Id { get; set; }
+        public string TagName { get; set; }
+    }
+}
